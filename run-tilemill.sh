7614a4df1e94af865a640ee899d566bfcab3e893
chrisnatali <chris.natali@gmail.com>
2016-02-04 17:28:19 -0500

    address CORS problem via new environment var

--- run-tilemill.sh
+++ run-tilemill.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-TILEMILL_HOST=`ifconfig eth0 | grep 'inet addr:'| cut -d: -f2 | awk '{ print $1}'` 
+# TILEMILL_HOST needs to be set approp. for client ajax requests
+if [[ -z "$TILEMILL_HOST" ]]
+then
+    TILEMILL_HOST=`ifconfig eth0 | grep 'inet addr:'| cut -d: -f2 | awk '{ print $1}'`
+fi
+
 /tilemill/index.js --server=true --listenHost=0.0.0.0 --coreUrl=${TILEMILL_HOST}:20009 --tileUrl=${TILEMILL_HOST}:20008
